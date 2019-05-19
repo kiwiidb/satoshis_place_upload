07db2da1d6745cad7b746c05713cf842bc54ef56
Kwinten De Backer <kwinten.de.backer@be-mobile.com>
2019-05-19 12:23:10 +0200

    working

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UploadSingleFile />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UploadSingleFile from './components/UploadSingleFile.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    UploadSingleFile
   }
 }
 </script>
