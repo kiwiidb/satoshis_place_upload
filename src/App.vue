62c43b8bb001ef1ba9afc9c411e9d75ceaf93770
Kwinten De Backer <kwinten.de.backer@be-mobile.com>
2019-05-19 18:17:42 +0200

    explaining text

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <p>
+    Here you can upload files to <a href="https://satoshis.place"> Satoshi's Place.</a>
+    <br>
+    Upload your file and a QR-code / lightning invoice will appear (if all goes well).
+    <br>
+    I do not charge any fees, the invoice is paid directly to Satoshi's Place.
+    <br>
+    X and Y coordinates are for the top left corner. Works best with .jpg images.
+    <br>
+    If you found this tool useful, please consider <a href="https://tippin.me/@kiwiidb"> tippin me some ⚡.</a>
+    </p>
     <UploadSingleFile />
   </div>
 </template>
